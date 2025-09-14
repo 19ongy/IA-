82644003fcdf63ca313d4d83ce4596b0c993aa9e
19ongy <19ong_y@bexleygs.co.uk>
2025-09-14 20:21:05 +0100

    finishing stuff up, added calendar numbesr and daily overviews when u click on the days

--- src/CalendarPanel.java
+++ src/CalendarPanel.java
@@ -60,9 +60,17 @@ public class CalendarPanel extends JPanel{
             monthLabel.setText(currentMonth.getMonth().toString() + " " + currentMonth.getYear());
             populateCalendar();
         });
+
+        JButton returnButton = new JButton("Return");
+        returnButton.setFocusPainted(false);
+        returnButton.setBackground(new Color(27,77,62));
+        returnButton.setForeground(Color.WHITE);
+        returnButton.addActionListener(e -> {
+            gui.setGraphMenu();
+        });
+        add(returnButton, BorderLayout.SOUTH);
     }
 
-    //TODO have to put the numbers in the top left of the calendar
     //filling up the calendar with the average moods
     private void populateCalendar(){
         calendarGrid.removeAll();
@@ -82,13 +90,101 @@ public class CalendarPanel extends JPanel{
             ArrayList<SessionManager> daySessions = SessionManager.getSessionsByDate(allSessions, date);
             String avgMoodEmoji = calculateAverageMood(daySessions);
 
-            JButton dayButton = new JButton(avgMoodEmoji);
+            JButton dayButton = new JButton();
+            dayButton.setLayout(null);
             dayButton.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 24));
             dayButton.setBackground(new Color(27,77,62));
             dayButton.setForeground(Color.WHITE);
             dayButton.setFocusPainted(false);
+            dayButton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+            dayButton.setPreferredSize(new Dimension(100, 100));
 
-            //TODO click the dates to open daily overview
+            //SETTING THE DATE
+            JLabel dateLabel = new JLabel(String.valueOf(day));
+            dateLabel.setFont(new Font("Arial", Font.BOLD, 12));
+            dateLabel.setForeground(Color.WHITE);
+            dateLabel.setBounds(5, 5, 30, 20);
+            dayButton.add(dateLabel);
+
+            //THE MOOD ON EACH DAY PANEL
+            JLabel moodLabel = new JLabel(avgMoodEmoji, SwingConstants.CENTER);
+            moodLabel.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 18));
+            moodLabel.setBounds(35, 30, 30, 30);
+            dayButton.add(moodLabel);
+
+            //STUDY TIME OF EACH DAY
+            int totalSeconds = 0;
+            for(SessionManager session : daySessions){
+                totalSeconds = session.sessionLength + totalSeconds;
+            }
+            double hours = totalSeconds/3600.0;
+            String hoursStudied = String.format("%.1f hrs", hours);
+            JLabel timeLabel = new JLabel(hoursStudied, SwingConstants.CENTER);
+            timeLabel.setFont(new Font("Arial", Font.PLAIN, 11));
+            timeLabel.setForeground(Color.WHITE);
+            timeLabel.setBounds(20, 65, 60, 20);
+            dayButton.add(timeLabel);
+
+            //action listener, they can click on the calendar panel and get a daily overview of that day
+            dayButton.addActionListener( e-> {
+                int totSeconds = 0;
+                String subject;
+                String mood;
+                HashMap<String, Integer> subjectTimeMap = new HashMap<>();
+                HashMap<String, Integer> moodMap = new HashMap<>();
+
+                //subject time aggregation
+                for(SessionManager session : daySessions){
+                    totSeconds = totSeconds + session.sessionLength;
+                    if(session.getSubject() != null){
+                        subject = session.getSubject();
+                    }else{
+                        subject = "Unknwon";
+                    }
+                    subjectTimeMap.put(subject, subjectTimeMap.getOrDefault(subject, 0) + session.sessionLength);
+
+                    //mood aggre gation
+                    if(session.getMoodBefore() != null){
+                        mood = session.getMoodBefore().toString();
+                    }else{
+                        mood = "SKIP";
+                    }
+                    moodMap.put(mood, moodMap.getOrDefault(mood, 0) + 1);
+                }
+
+                StringBuilder tableBuilder = new StringBuilder();
+                tableBuilder.append(date.toString() + "\n");
+                tableBuilder.append(" Total Studied: " + String.format("%.1f hrs", totSeconds / 3600.0) + "\n\n");
+                tableBuilder.append("Subjects:\n");
+
+                for(String subj : subjectTimeMap.keySet()){
+                    double hrs = subjectTimeMap.get(subj) / 3600.0;
+                    tableBuilder.append(" - " + subj + " : " + String.format("%.1f hrs", hrs) + "\n");
+
+                }
+
+                JTextArea popUp = new JTextArea(tableBuilder.toString());
+                popUp.setEditable(false);
+                popUp.setFont(new Font("Arial", Font.PLAIN, 14));
+                popUp.setBackground(new Color(27,77,62));
+                popUp.setForeground(Color.WHITE);
+                popUp.setMargin(new Insets(10 ,10, 10, 10));
+
+                JScrollPane scrollPane = new JScrollPane(popUp);
+                scrollPane.setPreferredSize(new Dimension(300, 250));
+
+                //allows it to pop up in the top right corner
+                JDialog dialog = new JDialog((Frame) null, "Daily Overview", false);
+                //puts a scrollable summary inside the dialogue
+                dialog.getContentPane().add(scrollPane);
+                dialog.pack();
+
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                dialog.setLocation(screenSize.width - dialog.getWidth() - 20, 20);
+
+                dialog.setVisible(true);
+            });
+
             calendarGrid.add(dayButton);
         }
         calendarGrid.revalidate();
